79d281e76d533de62277ff7c430cdaefad3d0002
anonymous <68572566+kinsingo@users.noreply.github.com>
2026-04-23 01:32:38 +0900

    Update make_calib.py

--- example/Mobilint-ARIES/compile/objectdetection/make_calib.py
+++ example/Mobilint-ARIES/compile/objectdetection/make_calib.py
@@ -1,13 +1,13 @@
-from qubee.calibration import make_calib_man
+#compiler version v1.1.2
+
+from qbcompiler.calibration import make_calib_man
 import cv2
 import numpy as np
-
 def preprocess_yolo(img_path: str):
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = img.astype(np.float32) / 255.
     return img
-
 make_calib_man(
     pre_ftn=preprocess_yolo,
     data_dir="./Calibration_Images/",
